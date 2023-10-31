c2e3eb4a86a6446816783e8dfd38663a634fffea
jahid4striver <jahidhassan701@gmail.com>
2023-10-31 07:24:41 +0600

    Watch gallary app router

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center text-6xl items-center h-[100vh]'>
+        <h1></h1>Loading...
+    </div>
+    );
+};
+
+export default Loading;
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className='flex justify-center text-6xl items-center h-[100vh]'>
+            <h1>404 Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
